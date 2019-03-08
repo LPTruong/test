83e14f48dc3f1b8f023b90f84532d03435ff9e0b
truong <phitruongvp247@gmail.com>
2019-03-08 15:12:56 +0700

    sdl project

---
+++ sdl/Untitled2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <SDL.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+void logSDLError(std::ostream& os,
+                 const std::string &msg, bool fatal = false);
+
+void logSDLError(std::ostream& os,
+                 const std::string &msg, bool fatal)
+{
+    os << msg << " Error: " << SDL_GetError() << std::endl;
+    if (fatal) {
+        SDL_Quit();
+        exit(1);
+    }
+}
